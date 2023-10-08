7eb13814fa7fad65aaf81b0c70c96e2253f05c7b
Nathan Kerns <nmk456@gmail.com>
2023-10-08 11:21:01 -0400

    Started PRN generator

---
+++ hw/spinal/gps/PRN.scala
@@ -0,0 +1,97 @@
+package gps
+
+import spinal.core._
+import spinal.lib._
+
+// Sample ratio is number of samples per code chip
+case class PRN() extends Component {
+  val io = new Bundle {
+    // Configuration
+    val set = in Bool()
+    val sv = in UInt(6 bits)
+    val inc = in UInt(17 bits)
+
+    // Output
+    val code = master Stream (Bool())
+    val code_count = out UInt(10 bits)
+    val sample_count = out UInt(16 bits)
+  }
+
+  // Flow control
+  io.code_count := 0
+  io.sample_count := 0
+
+  val chip_fraction = Reg(UInt(16 bits)) init U"16'h8000"
+  val increment = Reg(UInt(17 bits)) init 0
+  val chip_fraction_next = chip_fraction +^ increment
+  val advance_code = chip_fraction_next(16)
+  when(io.set) {
+    chip_fraction := U"16'h8000"
+    increment := io.inc
+  } otherwise {
+    chip_fraction := chip_fraction_next(15 downto 0)
+  }
+
+  // Code generation
+  val g1 = Reg(Bits(10 bits)) init B"10'h3FF"
+  val g1_new = g1(3-1) ^ g1(10-1)
+
+  val g2 = Reg(Bits(10 bits)) init B"10'h3FF"
+  val g2_new = g2(2-1) ^ g2(3-1) ^ g2(6-1) ^ g2(8-1) ^ g2(9-1) ^ g2(10-1)
+
+  val g2_tap1, g2_tap2 = Reg(UInt(4 bits)) init 0
+  val g2i = g2(g2_tap1 - 1) ^ g2(g2_tap2 - 1)
+  io.code.payload := g2i ^ g1(10-1)
+  io.code.valid := ~io.set & ~(g2_tap1 === 0)
+
+  // Set taps
+  when(io.set) {
+    switch(io.sv) {
+      is(1)(g2_tap1 := 2, g2_tap2 := 6)
+      is(2)(g2_tap1 := 3, g2_tap2 := 7)
+      is(3)(g2_tap1 := 4, g2_tap2 := 8)
+      is(4)(g2_tap1 := 5, g2_tap2 := 9)
+      is(5)(g2_tap1 := 1, g2_tap2 := 9)
+      is(6)(g2_tap1 := 2, g2_tap2 := 10)
+      is(7)(g2_tap1 := 1, g2_tap2 := 8)
+      is(8)(g2_tap1 := 2, g2_tap2 := 9)
+      is(9)(g2_tap1 := 3, g2_tap2 := 10)
+      is(10)(g2_tap1 := 2, g2_tap2 := 3)
+      is(11)(g2_tap1 := 3, g2_tap2 := 4)
+      is(12)(g2_tap1 := 5, g2_tap2 := 6)
+      is(13)(g2_tap1 := 6, g2_tap2 := 7)
+      is(14)(g2_tap1 := 7, g2_tap2 := 8)
+      is(15)(g2_tap1 := 8, g2_tap2 := 9)
+      is(16)(g2_tap1 := 9, g2_tap2 := 10)
+      is(17)(g2_tap1 := 1, g2_tap2 := 4)
+      is(18)(g2_tap1 := 2, g2_tap2 := 5)
+      is(19)(g2_tap1 := 3, g2_tap2 := 6)
+      is(20)(g2_tap1 := 4, g2_tap2 := 7)
+      is(21)(g2_tap1 := 5, g2_tap2 := 8)
+      is(22)(g2_tap1 := 6, g2_tap2 := 9)
+      is(23)(g2_tap1 := 1, g2_tap2 := 3)
+      is(24)(g2_tap1 := 4, g2_tap2 := 6)
+      is(25)(g2_tap1 := 5, g2_tap2 := 7)
+      is(26)(g2_tap1 := 6, g2_tap2 := 8)
+      is(27)(g2_tap1 := 7, g2_tap2 := 9)
+      is(28)(g2_tap1 := 8, g2_tap2 := 10)
+      is(29)(g2_tap1 := 1, g2_tap2 := 6)
+      is(30)(g2_tap1 := 2, g2_tap2 := 7)
+      is(31)(g2_tap1 := 3, g2_tap2 := 8)
+      is(32)(g2_tap1 := 4, g2_tap2 := 9)
+      default(g2_tap1 := 1, g2_tap2 := 1) // Shouldn't ever happen
+    }
+
+    g1 := B"10'h3FF"
+    g2 := B"10'h3FF"
+  }
+
+  when(advance_code & !io.set) {
+    g1 := g1(8 downto 0) ## g1_new
+    g2 := g2(8 downto 0) ## g2_new
+  }
+}
+
+object PrnVerilog extends App {
+  Config.spinal.generateVerilog(PRN())
+}
---
+++ hw/tb/PRN/test_prn.py
@@ -0,0 +1,60 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, ClockCycles, with_timeout
+from cocotbext import axi
+
+import logging
+import random
+import numpy as np
+import prn
+
+@cocotb.test()
+async def test_prn(dut):
+    cocotb.start_soon(Clock(dut.clk, period=10, units="ns").start())
+
+    axi_bus = axi.AxiStreamBus(dut)
+    axi_bus._add_signal("tdata", "io_code_payload")
+    axi_bus._add_signal("tvalid", "io_code_valid")
+    axi_bus._add_signal("tready", "io_code_ready")
+    axi_output = axi.AxiStreamSink(axi_bus, dut.clk, dut.reset, byte_size=1)
+    axi_output.log.setLevel(logging.WARNING) # Get rid of log messages
+
+    dut.reset.value = 1
+    dut.io_sv.value = 0
+    dut.io_set.value = 0
+
+    await ClockCycles(dut.clk, 2)
+
+    dut.reset.value = 0
+
+    for divider in [1.0, 5/3, 2.9999, 4]:
+        # for sv in range(1, 33):
+        for sv in range(1, 3):
+            dut.io_sv.value = sv
+            dut.io_inc.value = int(2**16 / divider)
+            dut.io_set.value = 1
+
+            await RisingEdge(dut.clk)
+
+            dut.io_set.value = 0
+
+            code_recv = []
+            code_len = int(np.ceil(1023*divider))
+            # code_ref = np.array(prn.generate(sv))
+            code_ref = np.array(prn.sample(sv, 1.023e6*divider, code_len).real, dtype=int)
+            # code_ref[code_ref == -1] = 0
+
+            for _ in range(code_len):
+                frame = await with_timeout(axi_output.recv(), 100, "ns")
+                code_recv.append(frame.tdata[0])
+            
+            code_recv = np.array(code_recv)
+            code_recv[code_recv == 0] = -1
+
+            corr = np.correlate(code_ref, code_recv)
+            assert np.max(corr) > code_len * 0.99, f"{np.max(corr)/code_len}, {divider}, {sv}"
+
+            assert len(code_ref) == len(code_recv), f"{len(code_ref)}, {len(code_recv)}"
+            matched = code_ref == code_recv
+            wrong = int(np.argmin(matched))
+            assert matched.all(), f"{sv}, {divider}, {wrong}, {code_ref[wrong-3:wrong+3]}, {code_recv[wrong-3:wrong+3]}"

29dc28ea3e5f022777c856af11e5ad6cb19ca019
Nathan Kerns <nmk456@gmail.com>
2024-05-03 21:57:45 -0400

    Formatting

--- hw/spinal/gps/Decimate.scala
+++ hw/spinal/gps/Decimate.scala
@@ -5,44 +5,44 @@ import spinal.lib._
 
 case class Decimate(
     iq_size: Int = 8,
-    factor: Int = 8,
+    factor: Int = 8
 ) extends Component {
-    val io = new Bundle {
-        val iq_in = slave Stream (Complex(iq_size).asBits)
-        val iq_out = master Stream (Complex(iq_size).asBits)
-    }
+  val io = new Bundle {
+    val iq_in = slave Stream (Complex(iq_size).asBits)
+    val iq_out = master Stream (Complex(iq_size).asBits)
+  }
 
-    val iq_in = io.iq_in.payload.as(Complex(iq_size))
-    
-    val dec_counter = Counter(factor + 1)
-    val dec_size = iq_size + log2Up(factor)
-    val dec_sample = Reg(Complex(dec_size)) init Complex(dec_size).getZero
-    val dec_out = Complex(iq_size)
+  val iq_in = io.iq_in.payload.as(Complex(iq_size))
 
-    dec_out.re := dec_sample.re >> log2Up(factor)
-    dec_out.im := dec_sample.im >> log2Up(factor)
+  val dec_counter = Counter(factor + 1)
+  val dec_size = iq_size + log2Up(factor)
+  val dec_sample = Reg(Complex(dec_size)) init Complex(dec_size).getZero
+  val dec_out = Complex(iq_size)
 
-    io.iq_in.ready := dec_counter < factor
-    io.iq_out.valid := dec_counter === factor
-    io.iq_out.payload := dec_out.asBits
+  dec_out.re := dec_sample.re >> log2Up(factor)
+  dec_out.im := dec_sample.im >> log2Up(factor)
 
-    // If input is valid and we're still waiting on samples
-    when (io.iq_in.fire & dec_counter < factor) {
-        dec_sample.re := dec_sample.re + iq_in.re
-        dec_sample.im := dec_sample.im + iq_in.im
+  io.iq_in.ready := dec_counter < factor
+  io.iq_out.valid := dec_counter === factor
+  io.iq_out.payload := dec_out.asBits
 
-        dec_counter.increment()
-    }
+  // If input is valid and we're still waiting on samples
+  when(io.iq_in.fire & dec_counter < factor) {
+    dec_sample.re := dec_sample.re + iq_in.re
+    dec_sample.im := dec_sample.im + iq_in.im
 
-    // Wait for output to be consumed
-    when (io.iq_out.fire) {
-        dec_sample.re := 0
-        dec_sample.im := 0
+    dec_counter.increment()
+  }
 
-        dec_counter.clear()
-    }
+  // Wait for output to be consumed
+  when(io.iq_out.fire) {
+    dec_sample.re := 0
+    dec_sample.im := 0
+
+    dec_counter.clear()
+  }
 }
 
 object DecimateVerilog extends App {
-    Config.spinal.generateVerilog(Decimate())
+  Config.spinal.generateVerilog(Decimate())
 }

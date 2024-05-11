575c455838c2970940ed6abcec77bc0eae7ce213
nmk456 <nmk456@gmail.com>
2024-05-11 15:05:22 -0400

    Add comment on PRN SV and fix formatting

--- hw/spinal/gps/PRN.scala
+++ hw/spinal/gps/PRN.scala
@@ -7,14 +7,14 @@ import spinal.lib._
 case class PRN() extends Component {
   val io = new Bundle {
     // Configuration
-    val set = in Bool()
-    val sv = in UInt(6 bits)
-    val inc = in UInt(17 bits)
+    val set = in Bool ()
+    val sv = in UInt (6 bits) // An input of 0 means SV 1
+    val inc = in UInt (17 bits)
 
     // Output
     val code = master Stream (Bool())
-    val code_count = out UInt(10 bits)
-    val sample_count = out UInt(12 bits)
+    val code_count = out UInt (10 bits)
+    val sample_count = out UInt (12 bits)
   }
 
   val chip_fraction = Reg(UInt(16 bits)) init U"16'h8000"
@@ -23,7 +23,7 @@ case class PRN() extends Component {
   val advance_code = chip_fraction_next(16)
 
   val code_count = Counter(1023, advance_code)
-  val sample_count = Counter(4*1023, io.code.ready) // TODO: don't hard code sample rate
+  val sample_count = Counter(4 * 1023, io.code.ready) // TODO: don't hard code sample rate
 
   io.code_count <> code_count
   io.sample_count <> sample_count
@@ -38,25 +38,25 @@ case class PRN() extends Component {
     code_count.clear()
     sample_count.clear()
     debug_count.clear()
-  } elsewhen(io.code.ready) {
+  } elsewhen (io.code.ready) {
     chip_fraction := chip_fraction_next(15 downto 0)
   }
 
   // Code generation
   val g1 = Reg(Bits(10 bits)) init B"10'h3FF"
-  val g1_new = g1(3-1) ^ g1(10-1)
+  val g1_new = g1(3 - 1) ^ g1(10 - 1)
 
   val g2 = Reg(Bits(10 bits)) init B"10'h3FF"
-  val g2_new = g2(2-1) ^ g2(3-1) ^ g2(6-1) ^ g2(8-1) ^ g2(9-1) ^ g2(10-1)
+  val g2_new = g2(2 - 1) ^ g2(3 - 1) ^ g2(6 - 1) ^ g2(8 - 1) ^ g2(9 - 1) ^ g2(10 - 1)
 
   val g2_tap1, g2_tap2 = Reg(UInt(4 bits)) init 0
   val g2i = g2(g2_tap1 - 1) ^ g2(g2_tap2 - 1)
-  io.code.payload := g2i ^ g1(10-1)
+  io.code.payload := g2i ^ g1(10 - 1)
   io.code.valid := ~io.set & ~(g2_tap1 === 0)
 
   // Set taps
   when(io.set) {
-    switch(io.sv+1) {
+    switch(io.sv + 1) {
       is(1)(g2_tap1 := 2, g2_tap2 := 6)
       is(2)(g2_tap1 := 3, g2_tap2 := 7)
       is(3)(g2_tap1 := 4, g2_tap2 := 8)

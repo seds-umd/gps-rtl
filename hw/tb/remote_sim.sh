2e25adf490d93ca2ced604963703dfaf00c39203
Nathan Kerns <nmk456@gmail.com>
2024-05-19 22:34:20 -0400

    Fix scp issue in remote sim script

--- hw/tb/remote_sim.sh
+++ hw/tb/remote_sim.sh
@@ -14,7 +14,7 @@ git diff HEAD > .patch
 scp .patch ${REMOTE}:~/.remote_builds/${GIT_REPO_NAME}
 
 # Run sim on remote
-ssh ${REMOTE} << EOF
+ssh ${REMOTE} /bin/bash << EOF
     cd ~/.remote_builds/${GIT_REPO_NAME} &&
     source venv/bin/activate &&
     git reset --hard &&
@@ -31,7 +31,8 @@ SUCCESS=$?
 if [ $SUCCESS -eq 0 ]; then
     # Copy fst file back
     mkdir -p ./${SIM_NAME}/sim_build
-    scp "${REMOTE}:~/.remote_builds/${GIT_REPO_NAME}/hw/tb/${SIM_NAME}/sim_build/"'*.{fst,png}' ./${SIM_NAME}/sim_build/.
+    scp "${REMOTE}:~/.remote_builds/${GIT_REPO_NAME}/hw/tb/${SIM_NAME}/sim_build/"'*.fst' ./${SIM_NAME}/sim_build/.
+    scp "${REMOTE}:~/.remote_builds/${GIT_REPO_NAME}/hw/tb/${SIM_NAME}/sim_build/"'*.png' ./${SIM_NAME}/sim_build/.
 fi
 
 exit $SUCCESS

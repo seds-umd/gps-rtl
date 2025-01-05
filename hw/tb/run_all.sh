0c0a5b73061f0e4397b2fdba6d7e6050906664ae
Nathan Kerns <nmk456@gmail.com>
2025-01-05 16:48:34 -0500

    Add github actions regression tests

---
+++ hw/tb/run_all.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+RC=0
+
+# Catch errors
+trap 'RC=1' ERR
+
+DIRS=($SCRIPT_DIR/*)
+
+for dir in ${DIRS[@]}; do
+    if [ -d $dir ]; then
+        cd $dir
+        python test_*.py
+
+        if grep -q "failure" "sim_build/results.xml"; then
+            RC=1
+        fi
+    fi
+done
+
+exit $RC

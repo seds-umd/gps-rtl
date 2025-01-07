694b80ebe839027dd56e0e8c7029520639996965
Elif Ozaydin <elifozaydin23@gmail.com>
2025-01-06 19:27:51 -0500

    First byte repeats 3 times

--- hw/spinal/gps/StreamMemory.scala
+++ hw/spinal/gps/StreamMemory.scala
@@ -1,9 +1,9 @@
- package gps
+package gps
 
 import spinal.core._
 import spinal.lib._
 
-// TODO: make reads synchronous so BRAM can be used - right now it synthesizes to LUTRAM
+// Synchronous reads with BRAM
 
 case class StreamMemory[T <: Data](dataType: T, sizeLog: Int) extends Component {
   val io = new Bundle {
@@ -15,8 +15,8 @@ case class StreamMemory[T <: Data](dataType: T, sizeLog: Int) extends Component 
 
   val size = 1 << sizeLog
 
+  // input always valid
   io.input.ready := True
-  io.output.valid := True
 
   val mem = Mem(dataType, wordCount = size)
   mem.addAttribute("ram_style", "block")
@@ -25,9 +25,17 @@ case class StreamMemory[T <: Data](dataType: T, sizeLog: Int) extends Component 
   mem.write(wr_addr, io.input.payload, io.input.fire)
 
   val rd_addr_counter = Counter(size, io.output.fire)
-  val rd_addr = (rd_addr_counter.value + io.output_offset.asUInt) % size
-  io.output.fragment := mem.readAsync(rd_addr)
-  io.output.last := rd_addr_counter.willOverflowIfInc
+  val rd_addr = Reg(UInt(sizeLog bits)) init(0)
+  val rd_addr_mod = (rd_addr_counter.value + io.output_offset.asUInt) % size
+
+  when(io.output.fire) {
+    rd_addr := rd_addr_mod
+  }
+
+  val rd_data = mem.readSync(rd_addr, io.output.ready)
+  io.output.fragment := rd_data
+  io.output.valid := RegNext(io.output.ready, init = False)
+  io.output.last := RegNext(rd_addr_counter.willOverflowIfInc, init = False)
 }
 
 object StreamMemoryVerilog extends App {
